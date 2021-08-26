f01dcd8b8db189b2cf20780ad0c108e92a6b4bd5
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-08-26 13:27:04 +0800

    springboot自动配置特性

--- src/main/java/org/example/boot/MainApplication.java
+++ src/main/java/org/example/boot/MainApplication.java
@@ -1,13 +1,30 @@
 package org.example.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+/**
+ * @SpringBootApplication 声明该类为主配置类
+ */
+//@SpringBootApplication
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan("org.example.boot")
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class,args);
+        //1.返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class,args);
+
+        //2.查看容器里面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 
 }
